0cc844f04654986b17d70c576d79dc72a46f68a1
Satheesh <shakthi2704@gmail.com>
2025-03-04 11:21:00 +0530

    setup

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header/header"
+import React from "react"
+
+const Dashboard = () => {
+  return (
+    <div className="">
+      <div className="w-full max-w-sm">
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Label className="my-custom-class">Click me</Label>
+    </div>
+  )
+}
